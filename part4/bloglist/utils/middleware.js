1407783b5013a916ba600daeb7f1a2ee8df5dc43
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2024-08-02 21:03:09 +0300

    update

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -13,7 +13,7 @@ const requestLogger = (request, response, next) => {
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.startsWith('Bearer ')) {
-    request.token = authorization.replace("Bearer ", "")
+    request.token = authorization.replace('Bearer ', "")
   }
   next()
 }
