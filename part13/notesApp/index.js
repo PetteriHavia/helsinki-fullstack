1f306fdeaeb6766713c9f4bc3d2e2a517444cdd0
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2024-09-30 21:01:40 +0300

    Part13: exercises 13.1 - 13.4

---
+++ part13/notesApp/index.js
@@ -0,0 +1,22 @@
+//require('dotenv').config()
+const express = require("express");
+const app = express();
+
+const { PORT } = require("./util/config")
+const { connectToDatabase } = require("./util/db");
+
+const notesRouter = require("./controllers/notes");
+
+app.use(express.json());
+
+app.use("/api/notes", notesRouter);
+
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running in PORT: ${PORT}`)
+  })
+}
+
+start()
+
