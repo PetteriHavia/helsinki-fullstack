8a4266713d67238ae4a8a1b55ff5cb97da63c4d8
Petteri Havia <petsku15@hotmail.fi>
2024-10-07 21:26:10 +0300

    Part13 Exercises: 13.8 - 3.12

--- part13/notesApp/index.js
+++ part13/notesApp/index.js
@@ -1,15 +1,19 @@
 //require('dotenv').config()
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
 const { PORT } = require("./util/config")
-const { connectToDatabase } = require("./util/db");
+const { connectToDatabase } = require("./util/db")
 
-const notesRouter = require("./controllers/notes");
+const notesRouter = require("./controllers/notes")
+const userRouter = require("./controllers/users")
+const loginRouter = require("./controllers/login")
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/api/notes", notesRouter);
+app.use("/api/notes", notesRouter)
+app.use("/api/users", userRouter)
+app.use("/api/login", loginRouter)
 
 const start = async () => {
   await connectToDatabase()
